4eb9da070a9efb4f9161fdf5f72478f7081ab435
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function theBeatlesPlay(musicians, instruments) {
   return newArr
 }
 
-const facts = [
+var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",

cbb3dee3ec4a849a46840e266f2174606c95c077
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:48:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 
   var newArr = [];
 
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     newArr.push(musicians[i] + "plays" + instruments[i])
   }
   return newArr

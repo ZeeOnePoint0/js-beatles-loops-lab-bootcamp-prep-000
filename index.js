9d74ef01dc6181d216da323558fb5d5c8f875429
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:55:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,18 +13,21 @@ function theBeatlesPlay(musicians, instruments) {
   return newArr
 }
 
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+
 
 function johnLennonFacts(array) {
+  var facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+  var excla = [];
+  
   var i = 0;
 
   while (i -= facts.length) {
-    console.log(facts[i] + "!!!")
+    excla.push(facts[i] + "!!!");
   }
 
 
